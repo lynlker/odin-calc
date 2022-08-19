cf4d0edf9b1f5a447c19ffa31b722e5e2691e8ed
lynlker <nosrednassel@gmail.com>
2022-08-19 19:00:34 +0200

    refine html structure and add small js tweaks

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 // VARIABLES
+let firstValue = 0;
+let secondValue = 0;
+let isOpActive = false;
 
 
 // FUNCTIONS
 
+// basic operation functions
 function add(x, y){
     return x + y;
 }
-
 function subtract(x, y){
     return x - y;
 }
-
 function multiply(x, y){
     return x * y;
 }
-
 function divide(x, y){
     return x / y;
 }
-
 function operate(op, x, y){
     return op(x, y);
+}
+
+// button click function
+function whenClicked(btn){
+    
 }
 
 
